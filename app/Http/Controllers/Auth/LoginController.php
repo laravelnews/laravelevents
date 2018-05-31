212a2cc834d48214abc6107f3f68e3ca765fe82b
Eric L. Barnes <eric@ericlbarnes.com>
2018-05-31 18:24:42 -0400

    Trust all the proxies

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/approve';
 
     /**
      * Create a new controller instance.
