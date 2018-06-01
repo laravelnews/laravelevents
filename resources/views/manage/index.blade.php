b457d2d9740e7d54c7b750650d29cc932c0bf763
Eric L. Barnes <eric@ericlbarnes.com>
2018-06-01 09:17:38 -0400

    try out as a table

--- resources/views/manage/index.blade.php
+++ resources/views/manage/index.blade.php
@@ -5,11 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-sm-12 m-3 p-4 bg-white">
             <h3>Pending Events</h3>
-            @foreach ($pending as $event)
-                <li>{{ $event->title }}</li>
-            @endforeach
-        </div>
-
+            <table class="table table-striped">
+                @foreach ($pending as $event)
+                <tr>
+                    <td>{{ $event->title }}</td>
+                    <td>{{ $event->starts_at->format("M d, Y") }}</td>
+                    <td>{{ $event->location }}</td>
+                    <td>{{ $event->price }}</td>
+                </tr>
+                <tr>
+                    <td colspan="4">{{ $event->description }}</td>
+                </tr>
+                @endforeach
+            </table>
+            </div>
     </div>
 </div>
 @endsection
