7e486aa0a4a4e362eb51980990cab5b62f8fe8d1
Eric L. Barnes <eric@ericlbarnes.com>
2018-06-04 20:47:40 -0400

    Add links to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,11 @@
         <div class="col-lg-8 col-md-12 m-3">
             <div class="rounded card m-6 p-4 bg-white entry">
                 @if ($event->image)
-                <a href="/">
+                <a href="{{ $event->url }}">
                     <img src="/storage/{{ $event->image }}" alt="" style="margin-bottom: 20px;">
                 </a>
                 @endif
-                <h2><a href="/">{{ $event->title }}</a></h2>
+                <h2><a href="{{ $event->url }}">{{ $event->title }}</a></h2>
                 <p>{{ $event->description }}</p>
                 <div class="footer d-flex justify-content-around">
                     <div>
