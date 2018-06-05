22c6cfa0079ef63aa664386f03d2e472ed7d45f9
Eric L. Barnes <eric@ericlbarnes.com>
2018-06-04 20:11:28 -0400

    Adjust mail config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@laravel-news.com'),
+        'name' => env('MAIL_FROM_NAME', 'Laravel News'),
     ],
 
     /*
